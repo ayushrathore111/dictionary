806c253a9b7105fd183e7382a14b40d868483ef1
Ayush Rathore <94514710+ayushrathore111@users.noreply.github.com>
2022-09-16 20:30:18 +0530

    Add files via upload

---
+++ src/componenets/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return ( <
+        div className = "footer" >
+        <
+        hr style = {
+            { width: "90%", marginTop: 20 }
+        }
+        /> <
+        span className = "name" >
+        Made by { " " } <
+        a href = "https://ayushrathore111.github.io/portfolio/"
+        target = "__blank" >
+        Roadside Coder <
+        /a> < /
+        span > <
+        div className = "iconContainer" >
+        <
+        a href = "https://www.instagram.com/ayushrathore168/"
+        target = "__blank" >
+        <
+        i className = "fab fa-instagram-square fa-2x" > < /i> < /
+        a > <
+        a href = "https://www.linkedin.com/in/ayush-rathore-5404b0223/"
+        target = "__blank" >
+        <
+        i className = "fab fa-linkedin fa-2x" > < /i> < /
+        a > <
+        a href = "https://ayushrathore111.github.io/portfolio/"
+        target = "__blank" >
+        <
+        i className = "fab fa-youtube fa-2x" > < /i> < /
+        a > <
+        /div> < /
+        div >
+    );
+};
+
+export default Footer;
